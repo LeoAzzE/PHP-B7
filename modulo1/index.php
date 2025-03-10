6c0bd665a5e7a2188c1f8c389beb38b14b23c2e8
Borges dev <leosull50@hotmail.com>
2025-03-10 18:57:37 -0300

    iniciando no php

---
+++ modulo1/index.php
@@ -0,0 +1,6 @@
+<?php
+
+    $primeiroNome = 'Leo';
+    echo '<h1>Hello world!</h1>';
+    echo $primeiroNome
+?>
---
+++ modulo2/PHP+HTML/index.php
@@ -0,0 +1,21 @@
+<?php
+    $nome='LEO'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>misturando php com html</title>
+</head>
+<body>
+    <h1>Ola Mundo</h1>
+    <h2>Eu sou o 
+        <?php
+        echo $nome;
+        ?>
+
+    </h2>
+</body>
+</html>
